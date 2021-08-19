995cc4dda27e7dab65b942a575643b1c93987d55
Srijon Mallick <srijonmallick.2000@gmail.com>
2021-08-19 16:57:26 +0530

    Update test.php

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -1,4 +1,5 @@
 <?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
     class test extends CI_Controller{
         function index(){
             $this->load->view('themes/dashboard_employer');
