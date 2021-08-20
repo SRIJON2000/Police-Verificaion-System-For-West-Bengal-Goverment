6df69d122510088ab5c01abaf71e19d76cdafe76
Srijon Mallick <srijonmallick.2000@gmail.com>
2021-08-20 15:46:03 +0530

    Update dashboard_employer.php

--- application/views/themes/dashboard_employer.php
+++ application/views/themes/dashboard_employer.php
@@ -6,6 +6,7 @@ $this->load->view('layouts/header_view');
 <html>
 <head>
 	<title>Employer Dashboard</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <style>
 	.logout {
 		min-width: 180px;
