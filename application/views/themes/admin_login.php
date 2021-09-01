cc54bd39fccaca3ec11e8e0fc8e86a4b88f1474f
Srijon Mallick <srijonmallick.2000@gmail.com>
2021-09-01 20:55:38 +0530

    Update admin_login.php

--- application/views/themes/admin_login.php
+++ application/views/themes/admin_login.php
@@ -10,8 +10,7 @@ $this->load->library('session');
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Login Panel</title>
-
-<!--<link rel="stylesheet" href="<?php echo base_url(); ?>\application\views\themes\CSS\stylesheet1.css">-->
+<link rel="stylesheet" href="<?php echo base_url(); ?>\application\views\themes\CSS\stylesheet1.css">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
