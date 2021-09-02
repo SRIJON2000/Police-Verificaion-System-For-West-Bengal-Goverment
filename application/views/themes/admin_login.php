654863867e8d4f65dacc48b2d825b024cfefea33
Jit-Saha <43725078+Jit-Saha@users.noreply.github.com>
2021-09-02 15:05:33 +0530

    15:04 @Indranilsen123

--- application/views/themes/admin_login.php
+++ application/views/themes/admin_login.php
@@ -15,61 +15,29 @@ $this->load->library('session');
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-
-<style>
-	.login-box a{color: #0576d8;}
-	.btn-primary{background: #2576d8;border: none; padding:6px 15px; font-size:15px;}
-	.mt10{ margin-top:10px;}
-	.login-box .form-control{border: none;border-bottom: #999 1px solid;padding-left: 0;}
-	.login-box-body,.register-box-body{ padding:20px 30px;box-shadow: 2px 3px 3px rgba(0,0,0,0.4); -webkit-box-shadow: 2px 3px 3px rgba(0,0,0,0.4); -moz-box-shadow: 2px 3px 3px rgba(0,0,0,0.4); -o-box-shadow: 2px 3px 3px rgba(0,0,0,0.4);}
-	.login_btn {text-align: center;}
-	.login_btn_main {width: 30%; display: inline-block; margin-left: 5px; margin-top: 5px;}
-
-  .loader {
-  border: 5px solid #f3f3f3;
-  border-radius: 50%;
-  border-top: 5px solid #3498db;
-  width:30px;
-  height: 30px;
-  -webkit-animation: spin 2s linear infinite;
-  animation: spin 2s linear infinite;
-}
-.login_loader{display: table;margin-left: 40%;}
-.captchatxt {
-	position: absolute;
-	top: -21px;
-	width: 100%;
-	right: -22px;
-	}
-</style>
 </head>
 
 <body>
-<h3 class="danger">
-<?php 
-$error = $this->session->flashdata('error');
-if (!empty($error))
-  echo $error;
-  unset($_SESSION['error']);
-?>
-
-
-</h3>
 
 <div class="limiter">
 		<div class="container-login100">
-			<div class="wrap-login100 clearfix">
+			<div class="wrap-login100 clearfix text-center">
 
-      <span class="login100-form-title">Login for District Administration</span>
-
-      <p>Provide username and password for login</p>
+      <h2 class="login100-form-title">Login for District Administration</h2>
+	  <h3 class="text-center danger">
+		<?php 
+			$error = $this->session->flashdata('error');
+			if (!empty($error))
+  				echo $error;
+  			unset($_SESSION['error']);
+		?></h3>
+      <p class="text-center">Provide username and password for login</p>
 
 <?php echo (isset($error_message)) ? $error_message : "";?>
     <?php echo form_open('Login', array('class' => 'login_form','name' => 'login_form', 'id' => 'login_form','autocomplete'=>'off')); ?>
     
-    <div class="wrap-input100 validate-input " >
-							<input class="input100" type="text" name="login_id" value="<?php echo set_value('login_id'); ?>" placeholder="Username" placeholder="Username">
+    <div class=" wrap-input100 validate-input " >
+							<input class="text-center input100" type="text" name="login_id" value="<?php echo set_value('login_id'); ?>" placeholder="Enter Username" placeholder="Username">
 							<span class="focus-input100"></span>
 							<span class="symbol-input100">
 								<span class="lnr lnr-user"></span>
@@ -80,8 +48,8 @@ if (!empty($error))
        <?php echo form_error('login_id'); ?> 
      </div>
 
-		<div class="wrap-input100 validate-input"  >
-		  <input class="input100" type="password" name="password" placeholder="Password">
+		<div class="form-group mt-3 wrap-input100 validate-input"  >
+		  <input class="text-center input100" type="password" name="password" placeholder="Enter Password">
 		  <span class="focus-input100"></span>
 		  <span class="symbol-input100">
 		  <span class="lnr lnr-lock"></span>
@@ -115,13 +83,13 @@ if (!empty($error))
 	</div>
     
     <div class="container-login100-form-btn">
-		  <button class="login100-form-btn">Login</button>
+		  <button class="btn bg-success text-white login100-form-btn">Login</button>
 		</div>
      
     <div class="wrap-input100">
 		  	<div class="row">
-	         <div class="col-md-6 text-left">
-	             <a class="backto-txt"href="<?php echo base_url(); ?>"><span class="lnr lnr-arrow-left"></span> Back</a>
+	         <div class="col-md-1 text-left">
+	             <a class="btn bg-secondary text-light mb-4"href="<?php echo base_url(); ?>"><span class="lnr lnr-arrow-left"></span> Back</a>
 	            </div>
 	          </div>
 	       </div>
