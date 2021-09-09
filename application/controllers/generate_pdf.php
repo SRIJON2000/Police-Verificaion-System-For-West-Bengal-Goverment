4c9f3c7d6fcc584a5e33f28854669e34a8c43399
Rupak2580 <rupakpal10@gmail.com>
2021-09-10 01:01:55 +0530

    RUPAK
    01:01

--- application/controllers/generate_pdf.php
+++ application/controllers/generate_pdf.php
@@ -1,11 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+//include('vendor/autoload.php');
+require_once APPPATH.'third_party\vendor\autoload.php';
+ include_once APPPATH.'third_party\vendor\mpdf\mpdf\src\Mpdf.php';
+ 
 class Generate_pdf extends CI_Controller
 {
     function __construct()
 	{	
 		parent::__construct();
-		$this->load->model('generate_pdf_model');
+		//$this->load->model('generate_pdf_model');
 		
     }
 	public function generate_pdf()
@@ -18,3 +22,54 @@ class Generate_pdf extends CI_Controller
     }
 }
 ?>
+
+
+
+
+<?php 
+// require composer autoload
+// require __DIR__ . '/vendor/autoload.php';
+// $mpdf = new \Mpdf\Mpdf();
+
+// $url = urldecode($_REQUEST['url']);
+
+// // To prevent anyone else using your script to create their PDF files
+// if (!preg_match('@^https?://www\.mydomain\.com/@', $url)) {
+//     die("Access denied");
+// }
+
+// // For $_POST i.e. forms with fields
+// if (count($_POST) > 0) {
+
+//     $ch = curl_init($url);
+//     curl_setopt($ch, CURLOPT_HEADER, 0);
+//     curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1 );
+
+//     foreach($_POST as $name => $post) {
+//       $formvars = array($name => $post . " \n");
+//     }
+
+//     curl_setopt($ch, CURLOPT_POSTFIELDS, $formvars);
+//     $html = curl_exec($ch);
+//     curl_close($ch);
+
+// } elseif (ini_get('allow_url_fopen')) {
+//     $html = file_get_contents($url);
+
+// } else {
+//     $ch = curl_init($url);
+//     curl_setopt($ch, CURLOPT_HEADER, 0);
+//     curl_setopt ( $ch , CURLOPT_RETURNTRANSFER , 1 );
+//     $html = curl_exec($ch);
+//     curl_close($ch);
+// }
+
+// $mpdf = new \Mpdf\Mpdf();
+
+// $mpdf->useSubstitutions = true; // optional - just as an example
+// $mpdf->SetHeader($url . "\n\n" . 'Page {PAGENO}');  // optional - just as an example
+// $mpdf->CSSselectMedia='mpdf'; // assuming you used this in the document header
+// $mpdf->setBasePath($url);
+// $mpdf->WriteHTML($html);
+
+// $mpdf->Output();?>
