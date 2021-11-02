ea7e2bd2edd6e5caff69d6897df42c5aac727a99
Jit-Saha <43725078+Jit-Saha@users.noreply.github.com>
2021-11-03 02:53:07 +0530

    Application setup

--- application/views/themes/new_application.php
+++ application/views/themes/new_application.php
@@ -16,9 +16,6 @@ $d=date('d-m-y');
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo base_url();?>\application\views\themes\CSS\stylesheet1.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-
-</head>
- <body>
 <style>
 #myBtn {
     display: none;
@@ -39,43 +36,38 @@ $d=date('d-m-y');
   #myBtn:hover {
     background-color: #555;
   }
-  
-  .row_new_app{
-  padding-right: 5% !important;
-}
 @media only screen and (max-width: 400px) {
   [class*="row_new_app"]{
     padding-right: 0px !important;
   }
 }
-
-  hr{
-    width:90%; border-top:10px solid red ;margin:auto;
-  }
-  label{
-    font-size:20px;
-  }
-  .px-2{
-    height: 35px; font-size: 20px;
-  }
+.border_lt{
+  border-left: solid 10px green;
+}
 </style>
-  <h3 class="text-center">Application Form</h3>
+</head>
+<body>
+<div class="container">
+  <div class="mt-2">
+    <h2>Application Form</h2>
+    <hr></div>
+  <section class="">
   <form id="application" action="<?php echo base_url();?>Application/newapp" method="post">
 
   <div class="row row_new_app pl-0">
     <div class="col-lg-6 col-md-12 p-3">
-    <div class=" d-flex justify-content-start row">    
+      <div class="row">    
         <label class="col-lg-3 col-xs-8" for="rcptno"><b>Receipt No</b></label>
-        <input class="col-lg-3 text-center col-sm-4 m-1" style="width:250px;" type="text" id="rcptno" name="receiptno" value="<?php echo $receiptno ;?>" placeholder="Receipt No" readonly>
+        <input class="col-lg-4 col-sm-4" maxlength="100" type="text" id="rcptno" name="receiptno" value="<?php echo $receiptno ;?>" placeholder="Receipt No" readonly>
     </div></div>
-    <div class="col-lg-6 col-md-12 p-3"><div class="d-flex justify-content-start row">
-        <label for="rcptdate" class="col-lg-3 text-center col-xs-8"><b>Receipt Date</b></label>
-        <input class="col-lg-3 text-center col-sm-4 m-1" style="width:250px;" type="date" id="rcptdate"  value="<?php echo date('Y-m-d');?>" name="receiptdate" readonly>
+    <div class="col-lg-6 col-md-12 p-3"><div class="row">
+        <label for="rcptdate" class="col-lg-3  col-xs-8"><b>Receipt Date</b></label>
+        <input class="col-lg-4 col-sm-4" maxlength="150" type="date" id="rcptdate"  value="<?php echo date('Y-m-d');?>" name="receiptdate" readonly>
     </div></div>
-    <div class="col-lg-6 col-md-12 p-3"><div class="row d-flex justify-content-start">
-      <label for="employer" class="col-lg-3 text-center col-xs-8"><b>Office/Employer Name</b></label>
-      <select class="col-lg-3 text-center col-sm-4 m-1" style="width:250px;" name="employer" id="employer">
-          <option>--Select Office--</option>
+    <div class="col-lg-6 col-md-12 p-3"><div class="row">
+      <label for="employer" class="col-lg-3 col-xs-8"><b>Office/Employer Name</b></label>
+      <select class="col-lg-4 col-sm-4" maxlength="100" name="employer" id="employer">
+          <option>-Select Office-</option>
           <?php
             foreach($employers as $employer)
             {
@@ -88,46 +80,11 @@ $d=date('d-m-y');
       </select>
     </div>
     </div>
+  </div><br/><br/>
+    <hr>  
+  <div class="border_lt">  
+	<h3 class="px-3">Candidate Personal Details</h3>
   </div>
-    </div><br/><br/>
-  <!-- <div class="row_style">
-      <div class="col1">
-        <label for="officeaddress1"><b>Office Address1</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" style="width:780px;" id="officeaddress1" name="officeaddress1"  required>
-      </div>
-  </div><br/><br/><br/><br/>
-  <div class="row_style">
-      <div class="col1">
-        <label for="officeaddress2"><b>Office Address2</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" style="width:780px;" id="officeaddress2" name="officeaddress2"  required>
-      </div>
-  </div><br/><br/><br/><br/> -->
-
-
-	<!-- <div class="row_style">
-	<div class="col1">
-        <label for="authnm"><b>Authority Name</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" id="authnm" name="authorityname"  placeholder="Authority Name" required>
-      </div>
-    </div>
-
-	<div class="row_style">
-      <div class="col2">
-        <label for="authaddr" ><b>Authority Address</b></label>
-      </div>
-      <div class="col2_in">
-        <input type="text" style="width:780px;"id="authaddress" name="authorityaddress" placeholder="Authority Address" required>
-      </div>
-    </div><br/><br/><br/><br/><br/> -->
-    <hr><br/>
-    
-	<h3 class="text-danger text-center">Candidate Personal Details</h3><br/>
   <div class="row row_new_app">
     <div class="col-lg-3 col-md-12 p-3 pt-5">
         <label><b>Candidate Name</b></label>
@@ -145,24 +102,6 @@ $d=date('d-m-y');
         <input type="text" id="lname" name="lastname" placeholder="Your last name.." required>
       </div>
           </div>
-	<!-- <div class="row_style">
-      <div class="col1">
-        <label for="fathername"><b>Candidate's Father Name</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" id="fathername" name="fathername" placeholder="Father's Name..." required>
-      </div>
-    </div>
-
-  <div class="row_style">
-      <div class="col2">
-        <label for="nationality"><b>Nationality</b></label>
-      </div>
-      <div class="col2_in">
-        <input type="text" id="nation" name="nation" value="Indian" placeholder="Your nation.." required>
-      </div>
-  </div> -->
-
   <div class="row row_new_app">
       <div class="col-lg-3 col-md-12 p-3 pt-5">
         <label for="dob"><b>Date Of Birth</b></label>
@@ -170,7 +109,7 @@ $d=date('d-m-y');
       </div>  
       <div class="col-lg-3 col-md-12 p-3 pt-5">
         <label for="gender"><b>Gender</b></label>
-        <select class="col-5 text-center px-2" name="gender" style="width: 225px;" id="gender">
+        <select class="col-lg-10 p-1" maxlength="30" name="gender" id="gender">
           <option>----Select----</option>
           <?php
             foreach($genders as $gender)
@@ -183,31 +122,13 @@ $d=date('d-m-y');
           ?>
         </select>
       </div>  
-  <!-- <div class="row_style">
-      <div class="col1">
-        <label for="mothername"><b>Candidate's Mother Name</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" id="mothername" name="mothername" placeholder="Mother's Name..." required>
-      </div>
-  </div>
-
-  <div class="row_style">
-      <div class="col2">
-        <label for="pob"><b>Place Of Birth</b></label>
-      </div>
-      <div class="col2_in">
-        <input type="text" id="pob" name="pob" placeholder="POB" required>
-      </div>
-  </div> -->
-
       <div class="col-lg-3 col-md-12 p-3 pt-5">
         <label for="aadhar"><b>Aadhaar No</b></label>
         <input type="number" id="aadhar" name="aadhaarno" maxlength="12" placeholder="Aadhar number.." required>
       </div>
       <div class="col-lg-3 col-md-12 p-3 pt-5">
         <label for="caste"><b>Caste</b></label>
-      <select class="col-5 text-center px-2" name="caste" id="caste" style="width: 225px;">
+      <select class="col-lg-10 p-1" maxlength="30" name="caste" id="caste">
           <option>--Select--</option>
           <?php
             foreach($castes as $caste)
@@ -221,28 +142,12 @@ $d=date('d-m-y');
       </select>
       </div>
   </div>
-
-  <!-- <div class="row_style"> 
-      <div class="col3">
-        <label for="blood group"><b>Blood Group</b></label>
-      </div>
-      <div class="col3_in">
-      <select name="blood" id="blood">
-          <option>--Select--</option>
-          <option value="A+">A+</option>
-          <option value="A-">A-</option>
-          <option value="B+">B+</option>
-          <option value="B-">B-</option>
-          <option value="O+">O+</option>
-          <option value="O-">O-</option>
-          <option value="AB-">AB-</option>
-          <option value="AB+">AB+</option>
-      </select>
-      </div>
-  </div><br/><br/><br/><br/><br/> --><br/>
-  <hr size="8" width="90%" color="red">
+  <br/>
+  <hr>
   <br>
-	<h3 class="text-danger text-center">Candidate's Permanent Address</h3>
+  <div class="border_lt">
+	<h3 class="px-3">Candidate's Permanent Address</h3>
+  </div>
   <div class="row row_new_app">
       <div class="col-lg-4 col-md-12 col-sm-8 p-3 pt-5">
         <label for="houseno1"><b>House No</b></label>
@@ -319,12 +224,13 @@ $d=date('d-m-y');
         ?>
     </select>
       </div>
-  </div>
- </div><br/>
-  <hr size="8" width="90%" color="red">
+  </div><br/>
+  <hr>
   <br>
-  <h3 class="text-danger text-center" > Candidate's Current Address</h3>
-  <div class="text-danger text-center" ><input type="checkbox" id="check" onclick="show()">Same as Permanent Address</div>
+  <div class="border_lt">
+  <h3 class="px-3" > Candidate's Current Address</h3>
+  <div class="px-3" ><input type="checkbox" id="check" onclick="show()">Same as Permanent Address</div>
+  </div>
   <div class="row row_new_app">
       <div class="col-lg-4 col-md-12 p-3 pt-5">
         <label for="houseno2"><b>House No</b></label>
@@ -401,116 +307,24 @@ $d=date('d-m-y');
         ?>
     </select>
       </div>
-  </div></div>
+  </div>
     <br/><br/>
   
   <hr>
-	<!-- <p style="font-size:30px; text-align:center; color:red">Candidate's Educational Details</p><br/>
-  <hr size="1" width="90%" color="red">
-  <div style="text-align:center;"><b>School Details</b></div>
-  <hr size="1" width="90%" color="red">
-  <div class="row_style">
-      <div class="col1">
-        <label for="sname"><b>School Name</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" style="width:780px;" id="sname" name="sname" placeholder="Full Name..." required>
-      </div>
-  </div><br/><br/><br/><br/>
-  <div class="row_style">
-      <div class="col1">
-        <label for="saddress"><b>School Address</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" style="width:780px;" id="saddress" name="saddress" placeholder="Address..." required>
-      </div>
-  </div><br/><br/><br/><br/>
-  <div class="row_style">
-    <div class="col1">
-        <label for="spin"><b>School Pincode</b></label>
-      </div>
-    <div class="col1_in">
-        <input type="number" style="height: 40px; font-size: 20px"id="spin" name="spin"  placeholder="Pincode.." required>
-    </div>
-
-    <div class="col2">
-    <label for="state3"><b>State</b></label>
-    </div>
-    <div class="col2_in">
-    <select class="form-control" id="inputState3" name="sstate">
-                        <option value="">--------------------------Select State-------------------------</option>
-                        
-                      </select>
-  </div>
-  <div class="col3">
-    <label for="inputDistrict3"><b>District</b></label>
-  </div>
-  <div class="col3_in">
-    <select class="form-control" id="inputDistrict3" name="sdistrict">
-        <option value="">--------------------------Select State-------------------------</option>
-    </select>
-  </div>
-  </div><br/><br/><br/><br/>
-  <hr size="1" width="90%" color="red">
-  <div style="text-align:center;"><b>College Details</b></div>
-  <hr size="1" width="90%" color="red">
-  <div class="row_style">
-      <div class="col1">
-        <label for="cname"><b>College Name</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" style="width:780px;" id="cname" name="cname" placeholder="Full Name..." required>
-      </div>
-  </div><br/><br/><br/><br/>
-  <div class="row_style">
-      <div class="col1">
-        <label for="caddress"><b>College Address</b></label>
-      </div>
-      <div class="col1_in">
-        <input type="text" style="width:780px;" id="caddress" name="caddress" placeholder="Address..." required>
-      </div>
-  </div><br/><br/><br/><br/>
-  <div class="row_style">
-    <div class="col1">
-        <label for="cpin"><b>College Pincode</b></label>
-      </div>
-    <div class="col1_in">
-        <input type="number" style="height: 40px; font-size: 20px" id="cpin" name="cpin"  placeholder="Pincode.." required>
-    </div>
-
-    <div class="col2">
-    <label for="state4"><b>State</b></label>
-    </div>
-    <div class="col2_in">
-    <select class="form-control" id="inputState4" name="cstate">
-                        <option value="">--------------------------Select State-------------------------</option>
-                        
-                      </select>
-    </div>
-    <div class="col3">
-    <label for="inputDistrict4"><b>District</b></label>
-  </div>
-  <div class="col3_in">
-    <select class="form-control" id="inputDistrict4" name="cdistrict">
-        <option value="">--------------------------Select State-------------------------</option>
-    </select>
-  </div>
-  </div><br/><br/><br/><br/> --><br/>
-  <!-- <hr> -->
   <div class="row row_new_app">
       <div class="col-lg-6 col-md-12 p-3 pt-5">
         <label for="refno"><b>Reference No</b></label>
-        <input type="text" id="refno" name="refno" placeholder="--Ref No--" class="px-2 text-center" style="width: 225px;" required>
+        <input type="text" id="refno" name="refno" placeholder="--Ref No--" class="px-2" style="width: 225px;" required>
       </div>
       <div class="col-lg-6 col-md-12 p-3 pt-5">
         <label for="refdate"><b>Reference Date</b></label>
-        <input type="date" id="refdate" name="refdate" class="px-2 text-center" style="width: 225px;" required>
+        <input type="date" id="refdate" name="refdate" class="px-2" style="width: 225px;" required>
       </div>
     </div><br/>
     <div class="row row_new_app">
       <div class="col-lg-6 col-md-12 p-3 pt-5">
         <label for="defence"><b>Defence Personnel</b></label>
-      <select name="defence" id="defence" class="px-2 text-center" style="width: 225px;" required>
+      <select name="defence" id="defence" class="px-2" style="width: 225px;" required>
           <option>--Select--</option>
           <?php
                 foreach($defences as $defence)
@@ -539,15 +353,6 @@ $d=date('d-m-y');
       </select>
       </div> 
         </div>
-
-	<!-- <div class="row_style">
-      <div class="col3">
-        <label for="ps3" ><b>Police Station</b></label>
-      </div>
-      <div class="col3_in">
-        <input type="text" id="ps3" value="Howrah"name="ps3">
-      </div>
-    </div>-->
     <div class="row">
       <div class="d-lg-flex d-md-flex flex-md-row flex-sm-column justify-content-center col-lg-12 col-md-12 p-3 pt-5">
       <button class=" btn bg-danger text-light m-3 pr-3 pl-3" type="submit" value="Reset" onclick="reset()"><b>Reset</b></button>
@@ -559,6 +364,8 @@ $d=date('d-m-y');
     </div>
   </form><br/><br/>
   <button class="btn" onclick="topFunction()" id="myBtn" title="Go to top">Top ↑</button>
+  </section>
+  </div>
   <?php $this->load->view('layouts/footer_view'); ?>
 </body>
 <script>
