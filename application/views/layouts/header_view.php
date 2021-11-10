4727ec077b484cd247b1a089338a0a11f30a7eb6
Jit-Saha <43725078+Jit-Saha@users.noreply.github.com>
2021-11-10 16:42:06 +0530

    y

--- application/views/layouts/header_view.php
+++ application/views/layouts/header_view.php
@@ -13,9 +13,9 @@
 <!-- <div class="container"> -->
 <div class="header row" id="header_body">
   <div class="row" id="header_body">
-    <div class="col-lg-2 col-md-2 col-sm-1 d-inline p-5 pt-0 pb-0 p-xs-0 text-center"><img src="<?php echo base_url(); ?>/application/views/pics/biswa_bangla_lt.png" alt="Biswa Bangla picture" height="110" width="80"></div>
-    <div class="text-center col-lg-8 col-md-6 col-sm-3 d-inline pt-2 p-xs-0"><h1><strong>Police Verification Reporting System</strong></h1></div>
-    <div class="col-lg-2 col-md-2 col-sm-1 d-inline p-5 pt-0 pb-0 p-xs-0 mx-auto"><img src="<?php echo base_url(); ?>/application/views/pics/biswa_bangla_rt.png" alt="Biswa Bangla logo" height="110" width="155"></div>
+    <div class="col-md-2 col-sm-3 col-xs-3 d-inline p-5 pt-0 pb-0 p-xs-0 text-center"><img src="<?php echo base_url(); ?>/application/views/pics/biswa_bangla_lt.png" alt="Biswa Bangla picture" height="110" width="80"></div>
+    <div class="col-md-8 col-sm-6 col-xs-6 d-inline pt-2 p-xs-0 text-center"><h1><strong>Police Verification Reporting System</strong></h1></div>
+    <div class="col-md-2 col-sm-3 col-xs-3 d-inline p-5 pt-0 pb-0 p-xs-0 mx-auto"><img src="<?php echo base_url(); ?>/application/views/pics/biswa_bangla_rt.png" alt="Biswa Bangla logo" height="110" width="155"></div>
   </div>
   <div class="mt-3 p-3 m-xs-0 row" id="header_body">
     <div class="col-lg-1 col-sm-4"><a class="p-3 pr-5 pl-5 active text-center" href="http://localhost/pvr/" <?php if ($_SERVER['PHP_SELF']=='/pvr/index.php/test/dashboard_admin'){echo 'target="_blank"';};?>><b>Home</b></a></div>
