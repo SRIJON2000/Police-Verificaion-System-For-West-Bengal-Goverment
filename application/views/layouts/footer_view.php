3e78a77a10601fa00b013c7cc28b7cfe22e40547
Srijon Mallick <srijonmallick.2000@gmail.com>
2021-12-07 16:30:45 +0530

    na

--- application/views/layouts/footer_view.php
+++ application/views/layouts/footer_view.php
@@ -9,7 +9,7 @@
     <title>Document</title>
 </head>
 <body>
-<footer class="text-light p-5" style="background-color:black;height:200px">
+<footer class="text-light p-5" style="background-color:black">
     <div class="copyRightsText text-center p-5 h-100" style="font-size:20px">&copy; Developed and designed by 
     <a href="http://www.nic.in/" target="_blank" id="footer_link">National Informatics Centre</a><br/>
     <a class="btn text-center text-light" style="font-size:20px" href="<?php echo base_url(); ?>/Home/contact">
