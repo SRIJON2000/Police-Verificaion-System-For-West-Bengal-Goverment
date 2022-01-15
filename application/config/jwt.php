1e6c66e3d2ab034cdfef508b6c1b083d56a463fb
RoyPritam <proyhb9857@gmail.com>
2022-01-16 00:05:58 +0530

    API Working

--- application/config/jwt.php
+++ application/config/jwt.php
@@ -36,4 +36,4 @@ $config['token_header'] = 'authorization';
 | ( 1 Hour ) : 60 * 60     = 3600
 | ( 1 Minute ) : 60        = 60
 */
-$config['token_expire_time'] = 300;
+$config['token_expire_time'] = 600;
